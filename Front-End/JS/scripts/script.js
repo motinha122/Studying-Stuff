ac17f0524c13277c15532d8951ec762ddbbde6a1
Tiago Mota <timota132@gmail.com>
2022-02-14 01:20:24 -0300

    Desafio Calcular troco Parte 1

--- Front-End/JS/scripts/script.js
+++ Front-End/JS/scripts/script.js
@@ -1,3 +1,36 @@
+function calcular_troco(valor_pago, valor_compra) {
+
+    let troco = valor_pago - valor_compra    //calcula o troco
+    let cents = Math.round((troco % 1) * 100)  //calcular centavos
+    let reais = Math.floor(troco)                     //calcular reais   
+
+    let valor_notas = [100,50,20,10,5,2,1] //valor de cada nota
+    let valor_centavos = [50,25,10,5,1]    //valor de cada centavo
+
+    function calcular(obj) {
+
+        let qtd_notas = [0,0,0,0,0,0,0] //quantidade de notas
+        let qtd_centavos = [0,0,0,0,0] // quantidade de centavos
+
+        moeda = (obj == valor_notas) ? reais : cents;
+        array_qtd = (obj == valor_notas) ? qtd_notas : qtd_centavos;
+        let indice =0;
+        for (indice in obj) {
+            while (moeda != 0 && moeda >= obj[indice]) {
+                moeda -= obj[indice]
+                array_qtd[indice] += 1
+            }
+        }
+        obj == valor_notas ? console.log(valor_notas,qtd_notas) : console.log(valor_centavos,qtd_centavos);
+    }
+    calcular(valor_notas)
+    calcular(valor_centavos)
+}
+calcular_troco(100, 73.67)
+calcular_troco(50, 10)
+
+
+
 /*const btn = document.querySelector("#send");
 
 btn.addEventListener("click",function(e){
@@ -8,45 +41,3 @@ btn.addEventListener("click",function(e){
 });*/
 
 //Desafio Troco 
-
-function calcular_troco(valor_pago,valor_compra){
-
-    let troco = valor_pago - valor_compra  //calcula o troco
-    let cents = Math.round((troco % 1)*100) //calcular centavos
-    let reais = Math.floor(troco)   //calcular reais   
-
-    let notas = {   //Reais
-        'tipo':reais,
-        100:0,
-        50:0,
-        20:0,
-        10:0,
-        5:0,
-        2:0,
-        1:0,
-    }
-    let centavos = {    //centavos
-        'tipo':cents,
-        50:0,
-        25:0,
-        10:0,
-        5:0,
-        1:0
-    }
-
-    function calcular(obj){
-        let prop //propriedade do obj
-        let tipo = obj['tipo']
-    for (prop != 'tipo' in obj){
-
-        while(moeda != 0 && moeda >= valor_nota){
-            moeda -= valor_nota
-            tipo[valor_nota] +=1
-        }
-    }
-    }
-}
-
-calcular_troco(100,53.65)
-calcular_troco(50,10)
-
